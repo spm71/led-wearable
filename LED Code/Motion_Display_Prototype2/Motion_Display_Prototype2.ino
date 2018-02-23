dd69e4bbcdcb9a9464bf0f0756bfbbb46fbce5af
Sam Morrison <spm71@case.edu>
2018-02-23 14:14:54 -0500

    Added comments

--- LED Code/Motion_Display_Prototype2/Motion_Display_Prototype2.ino
+++ LED Code/Motion_Display_Prototype2/Motion_Display_Prototype2.ino
@@ -1,53 +1,53 @@
-/* Motion_Display_Prototype2
- * Description: Colors entire LED strip one color based on accelerometer data
+/******************************************************************************
+ * File name: Motion_Display_Prototype2
+ * Purpose: Colors entire LED strip one color based on accelerometer data
  * Author: Sam Morrison
- * Date last modified: 2/21/2018
- */
-
-#include <bitswap.h>
-#include <chipsets.h>
-#include <color.h>
-#include <colorpalettes.h>
-#include <colorutils.h>
-#include <controller.h>
-#include <cpp_compat.h>
-#include <dmx.h>
+ * 
+ * Created: 2/19/2018
+ * Date last modified: 2/23/2018
+ *
+ ******************************************************************************/
 #include <FastLED.h>
-#include <fastled_config.h>
-#include <fastled_delay.h>
-#include <fastled_progmem.h>
-#include <fastpin.h>
-#include <fastspi.h>
-#include <fastspi_bitbang.h>
-#include <fastspi_dma.h>
-#include <fastspi_nop.h>
-#include <fastspi_ref.h>
-#include <fastspi_types.h>
-#include <hsv2rgb.h>
-#include <led_sysdefs.h>
-#include <lib8tion.h>
-#include <noise.h>
-#include <pixelset.h>
-#include <pixeltypes.h>
-#include <platforms.h>
-#include <power_mgt.h>
 
-#define NUM_LEDS 10
-#define LED_PIN 2
+#define NUM_LEDS 6 //number of LED's on strip
+#define LED_PIN 2 //digital pin for data output
 
-CRGB led[NUM_LEDS];
+CRGB led[NUM_LEDS]; //create LED array
 
 void set_led(int x, int y, int z);
 
-void setup() {
+/*
+ * void setup();
+ * Description: initialize LED strip to green
+ * 
+ * Inputs: 
+ *      Parameters: none
+ *      
+ * Outputs:
+ *      Returns: void
+*/
+void setup() { 
   FastLED.addLeds<NEOPIXEL, LED_PIN>(led, NUM_LEDS);
   for (int i = 0; i < NUM_LEDS; i++) {
-    led[i] = CRGB(0, 255, 0);
+    led[i] = CRGB(0, 100, 0);
   }
   FastLED.show();
 }
 
-void set_led(int x, int y, int z) {
+/*
+ * void set_led(int x, int y, int z) 
+ * Description: set all LED's based on accelerometer input
+ * 
+ * Inputs: 
+ *      Parameters:
+ *          int x: x-axis of movement
+ *          int y: y-axis of movement
+ *          int z: z-axis of movement
+ *      
+ * Outputs:
+ *      Returns: void
+*/
+void set_led(int x, int y, int z) { 
   FastLED.addLeds<NEOPIXEL, LED_PIN>(led, NUM_LEDS);
   for (int i = 0; i < NUM_LEDS; i++) {
     led[i] = CRGB(x, y, z);
